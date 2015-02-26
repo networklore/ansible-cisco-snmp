07f955dd229b5d0ff0e54bd3edbfea01244f5f58
ogenstad <patrick@ogenstad.com>
2015-02-26 18:34:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,102 @@
+## About
+
+This repo contains [Ansible](https://github.com/ansible/ansible) modules which use SNMP to change configuration on Cisco devices. The repo is currently in a proof of concept stage to see how you can manage older devices (which doesn't have a fancy api) with modern IT automation tools.
+
+## Goal
+
+The goal of this project is to provide idempotent modules for older Cisco devices.
+
+## Alpha code
+
+Currently this is only a test and there's a good chance that a lot of the code will change.
+
+## Dependencies
+
+This module requires:
+
+* [pysnmp](http://pysnmp.sourceforge.net) 4.2.5 or later
+* [nelsnmp](https://github.com/networklore/nelsnmp)
+* A good old Cisco switch
+
+## Installation of Ansible module
+
+pip install --upgrade pysnmp
+
+I haven't created an installer for nelsnmp, I'm currently deciding on the structure of that module. Eventually it will be installable through pip. For now you can just download the files or clone its repo and add it to your Python path:
+
+export PYTHONPATH="$PYTHONPATH:/location/to/nelsnmp/"
+i.e.
+export PYTHONPATH="$PYTHONPATH:/Users/patrick/src/nelsnmp/"
+
+If you are running Ansible through a Python virtualenv you might need to change the ansible_python_interpreter variable. Check the hosts file in this repo for an example.
+
+## Configuration of Cisco device
+
+snmp-server community [write-community-string] rw [acl]
+
+## Demo
+
+Running the playbook the first time:
+
+$ ansible-playbook -i hosts example-playbooks/how-to/examples-vlan.yml
+
+PLAY [all] ********************************************************************
+
+TASK: [Ensure VLAN 10 is present and has the name INTERNAL] *******************
+changed: [172.29.50.5]
+
+TASK: [Ensure VLAN 12 is present and has the name GUESTS] *********************
+changed: [172.29.50.5]
+
+TASK: [Ensure that VLAN 40 is created] ****************************************
+ok: [172.29.50.5]
+
+TASK: [Remove VLAN 80 if it is present] ***************************************
+changed: [172.29.50.5]
+
+PLAY RECAP ********************************************************************
+172.29.50.5                : ok=4    changed=3    unreachable=0    failed=0
+
+Running the playbook a second time:
+
+$ ansible-playbook -i hosts example-playbooks/how-to/examples-vlan.yml
+
+PLAY [all] ********************************************************************
+
+TASK: [Ensure VLAN 10 is present and has the name INTERNAL] *******************
+ok: [172.29.50.5]
+
+TASK: [Ensure VLAN 12 is present and has the name GUESTS] *********************
+ok: [172.29.50.5]
+
+TASK: [Ensure that VLAN 40 is created] ****************************************
+ok: [172.29.50.5]
+
+TASK: [Remove VLAN 80 if it is present] ***************************************
+ok: [172.29.50.5]
+
+PLAY RECAP ********************************************************************
+172.29.50.5                : ok=4    changed=0    unreachable=0    failed=0
+
+
+## Todo
+
+* Error handling (currently nelsnmp doesn't report errors)
+* SNMPv3 (I have the code ready in the other [snmp module](http://networklore.com/ansible-snmp-facts/) I released, but it hasn't been ported) 
+* Ability to save running configuration to startup configuration
+
+## Known issues
+
+* Naming conflicts: If you try to add a vlan using a name which already exists the module won't pick this up. The vlan will keep it's old name or be created without a name
+* No checking if the provided vlan_id is a valid number. I.e. the module won't complain if you try to create a vlan with id 37812942
+* Internal documentation still points to the snmp_facts module
+
+## Potential roadmap
+
+* Change interfaces i.e. access/trunk port, vlan assignments, description, admin up/down
+* Handle configuraion backups
+* All other things which might be possible through SNMP
+
+## Feedback
+
+If you have any questions or feedback. Please send me a note over [at my blog](http://networklore.com/contact/) or submit an issue here at Github.
---
+++ library/cisco_snmp_vlan.py
@@ -0,0 +1,279 @@
+#!/usr/bin/python
+
+DOCUMENTATION = '''
+---
+
+module: cisco_snmp_vlan
+author: Patrick Ogenstad (@networklore)
+short_description: Create or delete vlans.
+description:
+    - Retrieve facts for a device using SNMP, the facts will be
+      inserted to the ansible_facts key.
+requirements:
+    - pysnmp
+    - nelsnmp
+options:
+    host:
+        description:
+            - Set to {{ inventory_hostname }}}
+        required: true
+    version:
+        description:
+            - SNMP Version to use, v2/v2c or v3
+        choices: [ 'v2', 'v2c', 'v3' ]
+        required: true
+    community:
+        description:
+            - The SNMP community string, required if version is v2/v2c
+        required: false
+    level:
+        description:
+            - Authentication level, required if version is v3
+        choices: [ 'authPriv', 'authNoPriv' ]
+        required: false
+    username:
+        description:
+            - Username for SNMPv3, required if version is v3
+        required: false
+    integrity:
+        description:
+            - Hashing algoritm, required if version is v3
+        choices: [ 'md5', 'sha' ]
+        required: false
+    authkey:
+        description:
+            - Authentication key, required if version is v3
+        required: false
+    privacy:
+        description:
+            - Encryption algoritm, required if level is authPriv
+        choices: [ 'des', 'aes' ]
+        required: false
+    privkey:
+        description:
+            - Encryption key, required if version is authPriv
+        required: false
+'''
+
+EXAMPLES = '''
+# Create or delete VLANs on Cisco IOS through SNMP set
+- cisco_snmp_vlan: host={{ inventory_hostname }} version=2c community=public
+
+# Gather facts using SNMP version 3
+- cisco_snmp_vlan:
+    host={{ inventory_hostname }}
+    version=v3
+    level=authPriv
+    integrity=sha
+    privacy=aes
+    username=snmp-user
+    authkey=abc12345
+    privkey=def6789
+'''
+
+from ansible.module_utils.basic import *
+from collections import defaultdict
+
+try:
+    import nelsnmp.snmp
+    import nelsnmp.cisco_oids
+    from pysnmp.proto import rfc1902
+    g = nelsnmp.cisco_oids.CiscoOids("get")
+    s = nelsnmp.cisco_oids.CiscoOids("set")
+    v = nelsnmp.cisco_oids.CiscoOids("value")
+   
+    has_pysnmp = True
+except:
+    has_pysnmp = False
+
+def create_vlan(dev,vlan_id,vlan_name):
+    vlan_id = int(vlan_id)
+    
+    snmp_set = ((tuple(s.vtpVlanEditOperation + [1]), rfc1902.Integer('2')),
+                (tuple(s.vtpVlanEditBufferOwner + [1]), rfc1902.OctetString('Ansible'))
+            )    
+    dev.set(*snmp_set)  
+
+    #print "Row status"
+    snmp_set = (tuple(s.vtpVlanEditRowStatus + [1] + [vlan_id]), rfc1902.Integer('4'))
+    dev.set(snmp_set)
+
+    #print "VLAN type"
+    snmp_set = (tuple(s.vtpVlanEditType + [1] + [vlan_id]), rfc1902.Integer('1'))
+    dev.set(snmp_set)
+
+
+    #
+    if vlan_name != False:
+        snmp_set = (tuple(s.vtpVlanEditName + [1] + [vlan_id]), rfc1902.OctetString(vlan_name))
+        dev.set(snmp_set)
+
+
+    # Is this really needed?
+    #snmp_set = (tuple(s.vtpVlanEditDot10Said + [1] + [vlan_id]), rfc1902.OctetString('0x000186ab'))
+    #dev.set(snmp_set)
+
+
+    #print "Setting"
+    snmp_set = ((tuple(s.vtpVlanEditOperation + [1]), rfc1902.Integer('3')))
+
+    dev.set(snmp_set)
+
+    #print "Commiting"
+    snmp_set = ((tuple(s.vtpVlanEditOperation + [1]), rfc1902.Integer('4')))
+
+    dev.set(snmp_set)
+
+def delete_vlan(dev,vlan_id):
+    vlan_id = int(vlan_id)
+
+    snmp_set = (tuple(s.vtpVlanEditOperation + [1]), rfc1902.Integer('2'))
+                 
+    dev.set(snmp_set)
+
+    snmp_set = (tuple(s.vtpVlanEditRowStatus + [1] + [vlan_id]), rfc1902.Integer('6'))
+    dev.set(snmp_set)
+
+    snmp_set = ((tuple(s.vtpVlanEditOperation + [1]), rfc1902.Integer('3')))
+    dev.set(snmp_set)
+
+
+    snmp_set = ((tuple(s.vtpVlanEditOperation + [1]), rfc1902.Integer('4')))
+    dev.set(snmp_set)
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            host=dict(required=True),
+            version=dict(required=True, choices=['v2', 'v2c', 'v3']),
+            community=dict(required=False, default=False),
+            username=dict(required=False),
+            level=dict(required=False, choices=['authNoPriv', 'authPriv']),
+            integrity=dict(required=False, choices=['md5', 'sha']),
+            privacy=dict(required=False, choices=['des', 'aes']),
+            authkey=dict(required=False),
+            privkey=dict(required=False),
+            state=dict(required=True, choices=['absent', 'present']),
+            vlan_id=dict(required=True),
+            vlan_name=dict(required=False),
+            removeplaceholder=dict(required=False)),
+            required_together = ( ['username','level','integrity','authkey'],['privacy','privkey'],),
+        supports_check_mode=False)
+
+    m_args = module.params
+
+    if not has_pysnmp:
+        module.fail_json(msg='Missing required pysnmp module (check docs)')
+
+    #cmdGen = cmdgen.CommandGenerator()
+
+    # Verify that we receive a community when using snmp v2
+    if m_args['version'] == "v2" or m_args['version'] == "v2c":
+        if m_args['community'] == False:
+            module.fail_json(msg='Community not set when using snmp version 2')
+            
+    if m_args['version'] == "v3":
+        if m_args['username'] == None:
+            module.fail_json(msg='Username not set when using snmp version 3')
+
+        if m_args['level'] == "authPriv" and m_args['privacy'] == None:
+            module.fail_json(msg='Privacy algorithm not set when using authPriv')
+
+            
+        if m_args['integrity'] == "sha":
+            integrity_proto = cmdgen.usmHMACSHAAuthProtocol
+        elif m_args['integrity'] == "md5":
+            integrity_proto = cmdgen.usmHMACMD5AuthProtocol
+
+        if m_args['privacy'] == "aes":
+            privacy_proto = cmdgen.usmAesCfb128Protocol
+        elif m_args['privacy'] == "des":
+            privacy_proto = cmdgen.usmDESPrivProtocol
+    
+    # Use SNMP Version 2
+    if m_args['version'] == "v2" or m_args['version'] == "v2c":
+        dev = nelsnmp.snmp.SnmpHandler(version='2c', host=m_args['host'],
+                               community=m_args['community'])
+
+    # Use SNMP Version 3 with authNoPriv
+    elif m_args['level'] == "authNoPriv":
+        snmp_auth = cmdgen.UsmUserData(m_args['username'], authKey=m_args['authkey'], authProtocol=integrity_proto)
+
+    # Use SNMP Version 3 with authPriv
+    else:
+        snmp_auth = cmdgen.UsmUserData(m_args['username'], authKey=m_args['authkey'], privKey=m_args['privkey'], authProtocol=integrity_proto, privProtocol=privacy_proto)
+
+    #Tree = lambda: defaultdict(Tree)
+    changed_false = { 'changed': False }                           
+    changed_true = { 'changed': True }                           
+    #results = Tree()
+    vlan_defined_name = False
+
+    oids = []
+    oids.append(g.vtpVlanState)
+    if m_args['vlan_name']:
+        oids.append(g.vtpVlanName)
+        vlan_defined_name = m_args['vlan_name']
+    exists_vlan_id = False
+    exists_vlan_name = False
+    vartable = dev.getnext(*oids)
+    for varbinds in vartable:
+        for oid, val in varbinds:
+           
+            current_oid = oid.prettyPrint()
+            current_val = val.prettyPrint()
+            if v.vtpVlanState in current_oid:
+                vlan_id = current_oid.rsplit('.', 1)[-1]
+                if vlan_id == m_args['vlan_id']:
+                    exists_vlan_id = True
+            if v.vtpVlanName in current_oid:
+                vlan_id = current_oid.rsplit('.', 1)[-1]
+                if vlan_id == m_args['vlan_id']:
+                    if m_args['vlan_name'] == current_val:
+                        exists_vlan_name = True
+    
+    return_status = changed_true
+
+
+    if m_args['state'] == "present":
+        if m_args['vlan_name'] and exists_vlan_name:
+            return_status = changed_false
+            desired_state = True
+        elif m_args['vlan_name'] and not exists_vlan_name:
+            desired_state = False
+        elif exists_vlan_id:
+            return_status = changed_false
+            desired_state = True
+        else:
+            desired_state = False
+
+
+
+
+    if m_args['state'] == "absent":
+        if exists_vlan_id:
+            desired_state = False
+        else:
+            desired_state = True
+            return_status = changed_false
+
+    if not desired_state:
+        vartable = dev.getnext(g.vtpVlanEditTable)
+        if len(vartable) > 0:
+            module.fail_json(msg='Other changes are being made to the vlan database')
+
+        if m_args['state'] == "present":
+            # Create vlan
+            create_vlan(dev,m_args['vlan_id'],vlan_defined_name)
+        else:
+            # Remove vlan
+            delete_vlan(dev, m_args['vlan_id'])
+
+
+ 
+    module.exit_json(**return_status)
+    
+
+main()
+
